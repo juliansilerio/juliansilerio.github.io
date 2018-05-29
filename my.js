444adbddd9fd7384598b813ed60fa136455f103b
Julian Silerio <julian.silerio@gmail.com>
2018-05-29 04:04:16 -0700

    disable navbar (temporarily), remove bootstrap grid

--- my.js
+++ my.js
@@ -1,17 +1,21 @@
+function checkNavbar(location) {
+    console.log($(window).scrollTop());
+    if ($(window).scrollTop() > location) {
+        $('#nav_bar').show();
+        $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < location) {
+        $('#nav_bar').hide();
+        $('#nav_bar').removeClass('navbar-fixed');
+    }
+}
+
+
 $(document).ready(function() {
+    var LOCATION = 1000;
+
     console.log("ready");
     $(window).scroll(function () {
-        //if you hard code, then use console
-        //.log to determine when you want the
-        //nav bar to stick.
-        console.log($(window).scrollTop())
-        if ($(window).scrollTop() > 1000) {
-            $('#nav_bar').display = 'block';
-            $('#nav_bar').addClass('navbar-fixed');
-        }
-        if ($(window).scrollTop() < 1001) {
-            $('#nav_bar').display='none';
-            $('#nav_bar').removeClass('navbar-fixed');
-        }
+        checkNavbar(LOCATION);
     });
 });
